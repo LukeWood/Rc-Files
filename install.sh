2503703edde0ed59d28588fb29114520e16a4674
Luke Wood <lukewoodcs@gmail.com>
2022-02-15 04:01:58 +0000

    Add install

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git config --global credential.helper store
+sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 cp .zshrc ~/.zshrc
