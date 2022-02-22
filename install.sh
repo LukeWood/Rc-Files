61c84e74f990a1200b60eab20b4d7a94b5e24753
Luke Wood <lukewoodcs@gmail.com>
2022-02-22 14:49:41 -0800

    Fix dot-files install script

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 git config --global credential.helper store
-sudo apt install zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-cp .zshrc ~/.zshrc
-cp .vimrc ~/.vimrc
-cp .bashrc ~/.bashrc
+# sudo apt install zsh
+# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+echo "source $SCRIPT_DIR/.zshrc" >> $HOME/.zshrc
+echo "source $SCRIPT_DIR/.vimrc" >> $HOME/.vimrc
+echo "source $SCRIPT_DIR/.bashrc" >> $HOME/.bashrc
+
 cp .tmux.conf ~/.tmux.conf
