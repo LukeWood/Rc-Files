e57168b0340eef3fa72a8b4b07fea2072e923f8e
Luke Wood <lukewoodcs@gmail.com>
2023-02-21 20:01:05 +0000

    update installer

--- install.sh
+++ install.sh
@@ -9,4 +9,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo "source $SCRIPT_DIR/.zshrc" >> $HOME/.zshrc
 echo "source $SCRIPT_DIR/.vimrc" >> $HOME/.vimrc
 echo "source $SCRIPT_DIR/.bashrc" >> $HOME/.bashrc
+
+cp "$SCRIPT_DIR/.tmux.conf" ~
 echo "source-file $SCRIPT_DIR/.tmux.conf" >> $HOME/.tmux.conf
